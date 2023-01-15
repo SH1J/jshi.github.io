306a5fc76b34fc772341874c8abd50f041ede33b
SH1J <44064860+SH1J@users.noreply.github.com>
2023-01-14 22:49:30 -0500

    Add files via upload

---
+++ Stonk.js
@@ -0,0 +1,96 @@
+
+new TradingView.widget(
+  {
+  "autosize": true,
+  "symbol": "AMEX:SPY",
+  "container_id": "TV_SPY",
+  "interval": "1",
+  "timezone": "America/New_York",
+  "theme": "dark",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "hide_top_toolbar": true,
+}
+  );
+
+new TradingView.widget(
+  {
+  "autosize": true,
+  "symbol": "NASDAQ:AAL",
+  "container_id": "TV_AAL",
+  "interval": "1",
+  "timezone": "America/New_York",
+  "theme": "dark",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "hide_top_toolbar": true,
+}
+  );
+
+new TradingView.widget(
+  {
+  "autosize": true,
+  "symbol": "NYSE:DAL",
+  "container_id": "TV_DAL",
+  "interval": "1",
+  "timezone": "America/New_York",
+  "theme": "dark",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "hide_top_toolbar": true,
+}
+  );
+
+new TradingView.widget(
+  {
+  "autosize": true,
+  "symbol": "NASDAQ:UAL",
+  "container_id": "TV_UAL",
+  "interval": "1",
+  "timezone": "America/New_York",
+  "theme": "dark",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "hide_top_toolbar": true,
+}
+  );
+
+new TradingView.widget(
+  {
+  "autosize": true,
+  "symbol": "NYSE:T",
+  "container_id": "TV_T",
+  "interval": "1",
+  "timezone": "America/New_York",
+  "theme": "dark",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "hide_top_toolbar": true,
+}
+  );
+
+new TradingView.widget(
+  {
+  "autosize": true,
+  "symbol": "NYSE:NIO",
+  "container_id": "TV_NIO",
+  "interval": "1",
+  "timezone": "America/New_York",
+  "theme": "dark",
+  "style": "1",
+  "locale": "en",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "hide_top_toolbar": true,
+}
+  );
